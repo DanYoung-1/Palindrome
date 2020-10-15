5e800ecf08f97bbe0bc0ee3575301e02f05ff59b
Daniel Young <Daniel Young>
2020-10-14 19:21:08 -0600

    Initial Commit

---
+++ Palindrome/Palindrome.h
@@ -0,0 +1,18 @@
+//
+//  Palindrome.h
+//  Palindrome
+//
+//  Created by Daniel Young on 10/14/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Palindrome.
+FOUNDATION_EXPORT double PalindromeVersionNumber;
+
+//! Project version string for Palindrome.
+FOUNDATION_EXPORT const unsigned char PalindromeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Palindrome/PublicHeader.h>
+
+
