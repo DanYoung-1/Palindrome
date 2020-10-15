b7b16fdf3d5b0feb372bc7ebccd2ef44d86c5d1d
Daniel Young <Daniel Young>
2020-10-14 21:04:04 -0600

    first

--- PalindromeTests/PalindromeTests.swift
+++ PalindromeTests/PalindromeTests.swift
@@ -8,26 +8,64 @@
 import XCTest
 @testable import Palindrome
 
-class PalindromeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+struct PalindromePrinter {
+    func check(s: String) -> String {
+        switch s.count {
+        case 0: return "False"
+        case 1: return "True"
+        default:
+            let midN = s.count % 2 == 0 ? (s.count / 2) : (s.count / 2) + 1
+            let mIndex = s.index(s.startIndex, offsetBy: midN)
+            let sub2Index = s.index(s.startIndex, offsetBy: midN+1)
+            if String(s[..<mIndex]) == String(s[sub2Index...].reversed()) {
+                return "True"
+            }
+            return "False"
         }
     }
+}
 
+class PalindromeTests: XCTestCase {
+    
+    func test_PrintFalseWhenDigitsCountEqualsZero() {
+        expect(input: "", result: "False")
+    }
+    
+    func test_PrintTrueWhenDigitsCountEqualsOne() {
+        expect(input: "1", result: "True")
+    }
+    
+    func test_PrintFalseWhenDigitsCountEqualsTwo() {
+        expect(input: "12", result: "False")
+    }
+    
+    func test_PrintTrueWhenDigitCountIsEvenPalindrome() {
+        ["1221","123321","12344321"].forEach {
+            expect(input: $0, result: "False")
+        }
+    }
+    
+    func test_PrintTrueWhenThreeDigitNonPalindrome() {
+        expect(input: "122", result: "False")
+    }
+    
+    func test_PrintTrueWhenOddDigitCountNonPalindrome() {
+        ["122","12345","7654321"].forEach {
+            expect(input: $0, result: "False")
+        }
+    }
+    func test_PrintTrueWhenPalindrome() {
+        ["121", "12345","7654321"].forEach {
+            expect(input: $0, result: "False")
+        }
+    }
+    
+    private func makeSUT() -> PalindromePrinter {
+        return PalindromePrinter()
+    }
+    
+    private func expect(input: String, result: String) {
+        let sut = makeSUT()
+        XCTAssertEqual(sut.check(s: input), result)
+    }
 }
